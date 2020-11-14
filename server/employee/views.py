e82a01053d9fec1cf930cda71c76ef20efd3110e
delrocks <60640628+delrocks@users.noreply.github.com>
2020-11-14 21:43:27 +0530

    added extends and registration
    made nav reusable

--- server/employee/views.py
+++ server/employee/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-
+from .models import Employee
 # Create your views here.
 
 def LoginView(request):
+    context={}
+    if(request.method=="POST"):
+        queryset=Employee.objects.all()
+        if request.POST.get('username') and request.POST.get('pwd') in queryset:
+            print("Login successfull!")
+            context = {"error": "Login successfull"}
+        else:
+            print("Login unsuccessfull!")
+            context ={"error":"Login unsuccessfull"}
+    return render(request,"login.html",context)
 
-    return render(request,"login.html",{})
-
+# def RegisterView(request):
+#     return render(request,"reg.html",{})
 def RegisterView(request):
+    if (request.method=="POST"):
+        fname =request.POST.get('fname')
+        lname =request.POST.get('lname')
+        email =request.POST.get('email')
+        contact =request.POST.get('contact')
+        username =request.POST.get('username')
+        pwd =request.POST.get('pwd')
+        dob =request.POST.get('dob')
+        bio =request.POST.get('bio')
+        jobrole =request.POST.get('jobrole')
+        Employee.objects.create(fname=fname,lname=lname,email=email,contact=contact,username=username,pwd=pwd,dob=dob,bio=bio,jobrole=jobrole)
     return render(request,"reg.html",{})
